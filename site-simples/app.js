975264b6702c6c8531c4bca5bedef30ff84bc67d
SrErickinho <124881560+SrErickinho@users.noreply.github.com>
2023-08-10 08:18:29 -0300

    Add files via upload

---
+++ site-simples/app.js
@@ -0,0 +1,16 @@
+'use strict'
+if (typeof foo !== 'undefined') {}
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+    console.log('current class name: ' + className);
+});
